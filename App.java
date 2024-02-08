23bab65d94025d5cbf664142f38664f61497b62b
JavierBenitez112 <javibenn@gmail.com>
2024-02-07 18:16:40 -0600

    Algoritmos Integrados

--- App.java
+++ App.java
@@ -1,24 +1,20 @@
 import GeneradorNumAleatorios.MyFileReader;
+import Sorts.Gnome;
+import Sorts.Quick;
+import Sorts.Radix;
 import Sorts.Bubble;
+
+import Sorts.Merge.MergeSortAlgorithm;
 
 public class App {
     public static void main(String[] args) {
         int[] numbers = MyFileReader.readNumbersFromFile("archiv.txt");
-        System.out.println("Números sin ordenar:");
-        printArray(numbers);
+        System.out.println("Archivo creado con exito!");
         
-        // Ordena los números usando Bubble Sort
+        Gnome.gnomeSort(numbers, numbers.length);
+        MergeSortAlgorithm.mergeSort(numbers);
+        Quick.quicksort(numbers, 0, numbers.length - 1);
+        Radix.radixsort(numbers, numbers.length);
         Bubble.bubbleSort(numbers, numbers.length);
-        
-        // Imprime los números ordenados
-        System.out.println("Números ordenados:");
-        printArray(numbers);
-    }
-    
-    public static void printArray(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
     }
 }
